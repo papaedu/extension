61c2cb1f9a21d0bfca96c51bb0976e7e89ebc4f8
张超君 <yoki.zhang@papaenglish.cn>
2020-05-11 10:28:10 +0800

    feat: 优化数据库连接

--- src/Support/DbConnection.php
+++ src/Support/DbConnection.php
@@ -39,6 +39,7 @@ class DbConnection
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'modes' => [],
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
