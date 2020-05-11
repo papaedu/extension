3d587db2559cc1323c6959e0b9a488d0ae01cfa2
张超君 <yoki.zhang@papaenglish.cn>
2020-05-11 10:28:04 +0800

    feat: 增加Sanctum中间件判断登录角色

---
+++ src/Middleware/SanctumGuard.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Papaedu\Extension\Middleware;
+
+use Closure;
+use Papaedu\Extension\Traits\PapaeduHelpers;
+
+class SanctumGuard
+{
+    use PapaeduHelpers;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $provider
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $provider = 'users')
+    {
+        $model = config("auth.providers.{$provider}.model");
+        if (!$request->user() instanceof $model) {
+            $this->response->errorUnauthorized();
+        }
+
+        return $next($request);
+    }
+}
