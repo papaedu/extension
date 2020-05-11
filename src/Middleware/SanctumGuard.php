175e343dfbd67fb04a8a3725fc7e6ed205f86caa
张超君 <yoki.zhang@papaenglish.cn>
2020-05-11 11:21:07 +0800

    feat: 优化sancutm.guard读取auth的default配置

--- src/Middleware/SanctumGuard.php
+++ src/Middleware/SanctumGuard.php
@@ -17,8 +17,13 @@ class SanctumGuard
      * @param  string  $provider
      * @return mixed
      */
-    public function handle($request, Closure $next, $provider = 'users')
+    public function handle($request, Closure $next, $provider = '')
     {
+        if (!$provider) {
+            $guard = config('auth.defaults.guard');
+            $provider = config("auth.guards.{$guard}.provider");
+        }
+
         $model = config("auth.providers.{$provider}.model");
         if (!$request->user() instanceof $model) {
             $this->response->errorUnauthorized();
