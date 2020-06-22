206a80d080010432e5d542406c35cb917b718915
张皮皮 <zhangpipi.o3o@gmail.com>
2020-06-22 10:33:35 +0800

    feat: 增加AES加密

---
+++ src/Support/AES.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Papaedu\Extension\Support;
+
+class AES
+{
+    /**
+     * @param  string  $data
+     * @return string
+     * @throws \Exception
+     */
+    public static function encrypt(string $data)
+    {
+        $encrypted = openssl_encrypt($data, 'AES-128-ECB', 'papaEnglish', OPENSSL_RAW_DATA);
+        if (false === $encrypted) {
+            throw new \Exception('AES encrypt error.');
+        }
+
+        return strtoupper(bin2hex($encrypted));
+    }
+
+    /**
+     * @param  string  $data
+     * @return string
+     * @throws \Exception
+     */
+    public static function decrypt(string $data)
+    {
+        $decrypted = openssl_decrypt(hex2bin($data), 'AES-128-ECB', 'papaEnglish', OPENSSL_RAW_DATA);
+        if (false === $decrypted) {
+            throw new \Exception('AES decrypt error.');
+        }
+
+        return $decrypted;
+    }
+}
