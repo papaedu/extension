71bef096a0ef4042f88f7fe09e91784400c9f1c9
张皮皮 <zhangpipi.o3o@gmail.com>
2020-06-22 16:03:17 +0800

    feat: 新增全局Casts

---
+++ src/Casts/File.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Papaedu\Extension\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Papaedu\Extension\Support\Disks\Disk;
+
+class File implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return int
+     */
+    public function get($model, string $key, $value, array $attributes)
+    {
+        return Disk::file()->url($value);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return float
+     */
+    public function set($model, string $key, $value, array $attributes)
+    {
+        return Disk::file()->parseUrl($value);
+    }
+}
