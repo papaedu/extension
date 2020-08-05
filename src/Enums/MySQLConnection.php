1bec654569b4bf5e9ba277a5aef4a240e0e96aff
张皮皮 <zhangpipi.o3o@gmail.com>
2020-08-05 16:29:49 +0800

    新增数据库连接

---
+++ src/Enums/MySQLConnection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Papaedu\Extension\Enums;
+
+final class MySQLConnection
+{
+    const Shop = 'mysql';
+
+    const Exercise = 'mysql_exercise';
+
+    const Exam = 'mysql_exam';
+
+    const Activity = 'mysql_activity';
+
+    const Question = 'mysql_question';
+
+    const Revision = 'mysql_revision';
+
+    const Abroad = 'mysql_abroad';
+
+    const Analytics = 'mysql_analytics';
+}
