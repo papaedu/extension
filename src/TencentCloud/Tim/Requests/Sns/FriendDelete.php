041b7d55fdffb7c1be7371192d7e7fd4bced806e
波波 <934472858@qq.com>
2021-03-04 17:43:58 +0800

    删除好友

---
+++ src/TencentCloud/Tim/Requests/Sns/FriendDelete.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Papaedu\Extension\TencentCloud\Tim\Requests\Sns;
+
+use Papaedu\Extension\TencentCloud\Tim\Requests\TimRequest;
+
+class FriendDelete extends TimRequest
+{
+    /**
+     * FriendDelete constructor.
+     *
+     * @param  string  $fromAccount
+     * @param  array  $toAccount
+     */
+    public function __construct(string $fromAccount, array $toAccount)
+    {
+        $this->setFromAccount($fromAccount)
+            ->setToAccount($toAccount);
+    }
+
+    /**
+     * @return string
+     */
+    public function getUri(): string
+    {
+        return 'v4/sns/friend_delete';
+    }
+
+    /**
+     * @param  string  $fromAccount
+     * @return $this
+     */
+    public function setFromAccount(string $fromAccount): FriendDelete
+    {
+        $this->setParameter('From_Account', $fromAccount);
+
+        return $this;
+    }
+
+    /**
+     * @param  array  $toAccount
+     * @return $this
+     */
+    public function setToAccount(array $toAccount): FriendDelete
+    {
+        $this->setParameter('To_Account', $toAccount);
+
+        return $this;
+    }
+
+    /**
+     * @param  string  $deleteType
+     * @return $this
+     */
+    public function setDeleteType(string $deleteType): FriendDelete
+    {
+        $this->setParameter('DeleteType', $deleteType);
+
+        return $this;
+    }
+}
