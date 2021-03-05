79a2bba52d1d0f9eefa537be6431da18770020c3
波波 <934472858@qq.com>
2021-03-05 16:04:14 +0800

    校验好友

---
+++ src/TencentCloud/Tim/Requests/Sns/FriendCheck.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Papaedu\Extension\TencentCloud\Tim\Requests\Sns;
+
+use Papaedu\Extension\TencentCloud\Tim\Requests\TimRequest;
+
+class FriendCheck extends TimRequest
+{
+    /**
+     * FriendCheck constructor.
+     *
+     * @param  string  $fromAccount
+     * @param  array  $toAccount
+     * @param  string  $checkType
+     */
+    public function __construct(string $fromAccount, array $toAccount, string $checkType)
+    {
+        $this->setFromAccount($fromAccount)
+            ->setToAccount($toAccount)
+            ->setCheckType($checkType);
+    }
+
+    /**
+     * @return string
+     */
+    public function getUri(): string
+    {
+        return 'v4/sns/friend_check';
+    }
+
+    /**
+     * @param  string  $fromAccount
+     * @return $this
+     */
+    public function setFromAccount(string $fromAccount): FriendCheck
+    {
+        $this->setParameter('From_Account', $fromAccount);
+
+        return $this;
+    }
+
+    /**
+     * @param  array  $toAccount
+     * @return $this
+     */
+    public function setToAccount(array $toAccount): FriendCheck
+    {
+        $this->setParameter('To_Account', $toAccount);
+
+        return $this;
+    }
+
+    /**
+     * @param  string  $checkType
+     * @return $this
+     */
+    public function setCheckType(string $checkType): FriendCheck
+    {
+        $this->setParameter('CheckType', $checkType);
+
+        return $this;
+    }
+}
