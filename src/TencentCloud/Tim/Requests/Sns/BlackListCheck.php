a8779bba73cdfcc6977480ac2ca1f3fe5c818955
波波 <934472858@qq.com>
2021-03-05 16:43:18 +0800

    校验黑名单

---
+++ src/TencentCloud/Tim/Requests/Sns/BlackListCheck.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Papaedu\Extension\TencentCloud\Tim\Requests\Sns;
+
+use Papaedu\Extension\TencentCloud\Tim\Requests\TimRequest;
+
+class BlackListCheck extends TimRequest
+{
+    /**
+     * BlockListCheck constructor.
+     *
+     * @param  string  $fromAccount
+     * @param  array  $toAccount
+     * @param  string  $checkType
+     */
+    public function __construct(string $fromAccount, array $toAccount, string $checkType)
+    {
+        $this->setFromAccount($fromAccount)->setToAccount($toAccount)->setCheckType($checkType);
+    }
+
+    /**
+     * @return string
+     */
+    public function getUri(): string
+    {
+        return 'v4/sns/black_list_check';
+    }
+
+    /**
+     * @param  string  $fromAccount
+     * @return $this
+     */
+    public function setFromAccount(string $fromAccount)
+    {
+        $this->setParameter('From_Account', $fromAccount);
+
+        return $this;
+    }
+
+    /**
+     * @param  array  $toAccount
+     * @return $this
+     */
+    public function setToAccount(array $toAccount)
+    {
+        $this->setParameter('To_Account', $toAccount);
+
+        return $this;
+    }
+
+    /**
+     * @param  string  $checkType
+     * @return $this
+     */
+    public function setCheckType(string $checkType)
+    {
+        $this->setParameter('CheckType', $checkType);
+
+        return $this;
+    }
+}
