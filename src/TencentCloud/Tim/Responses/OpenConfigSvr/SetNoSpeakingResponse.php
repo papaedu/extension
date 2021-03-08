f4ae9eb192babf7683e5d13e4c1aa8d277289304
张皮皮 <zhangpipi.o3o@gmail.com>
2021-03-08 13:07:11 +0800

    增加全局禁言管理接口

---
+++ src/TencentCloud/Tim/Responses/OpenConfigSvr/SetNoSpeakingResponse.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Papaedu\Extension\TencentCloud\Tim\Responses\OpenConfigSvr;
+
+use Papaedu\Extension\TencentCloud\Tim\Responses\TimResponse;
+
+class SetNoSpeakingResponse extends TimResponse
+{
+}
