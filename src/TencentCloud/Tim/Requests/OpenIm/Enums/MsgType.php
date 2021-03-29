57acd29d1dda5f39a0c16679088412e471b48aaf
张皮皮 <zhangpipi.o3o@gmail.com>
2021-03-29 15:58:25 +0800

    增加消息类型枚举

---
+++ src/TencentCloud/Tim/Requests/OpenIm/Enums/MsgType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Papaedu\Extension\TencentCloud\Tim\Requests\OpenIm\Enums;
+
+final class MsgType
+{
+    public const TEXT = 'TIMTextElem';
+
+    public const LOCATION = 'TIMLocationElem';
+
+    public const FACE = 'TIMFaceElem';
+
+    public const CUSTOM = 'TIMCustomElem';
+
+    public const SOUND = 'TIMSoundElem';
+
+    public const IMAGE = 'TIMImageElem';
+
+    public const FILE = 'TIMFileElem';
+
+    public const VIDEO_FILE = 'TIMVideoFileElem';
+}
