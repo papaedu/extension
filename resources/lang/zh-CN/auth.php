f33686320ed0d49ab1e4effff435ef966581b8c7
张皮皮 <zhangpipi.o3o@gmail.com>
2021-03-30 17:11:02 +0800

    该话术

--- resources/lang/zh-CN/auth.php
+++ resources/lang/zh-CN/auth.php
@@ -6,12 +6,12 @@ return [
     'failed' => '用户名或密码错误',
     'throttle' => '尝试登录次数过多，请:seconds秒后再试',
 
-    'registered' => '此:attribute已经注册',
-    'unregister' => '此:attribute尚未注册',
+    'registered' => '该:attribute已经注册',
+    'unregister' => '该:attribute尚未注册',
 
     'captcha_failed' => ':attribute错误',
     'onelogin_failed' => '一键登录失败',
 
-    'status_baned' => '此账号已封停',
-    'status_closed' => '此账号已注销',
+    'status_baned' => '该账号已封停',
+    'status_closed' => '该账号已注销',
 ];
