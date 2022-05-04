53aaafc29a4738fbf183ebc645f961422db07f46
zhangpipi <zhangpipi.o3o@gmail.com>
2022-05-04 16:45:54 +0800

    Add doc

--- src/Models/Methods/SocialiteMethod.php
+++ src/Models/Methods/SocialiteMethod.php
@@ -6,6 +6,15 @@ use Illuminate\Support\Facades\Redis;
 
 trait SocialiteMethod
 {
+    /**
+     * 设置临时缓存，用于后续绑定账号使用
+     *
+     * @param  string  $driver  驱动名称:wechat
+     * @param  int  $type  授权类型
+     * @param  string  $openid
+     * @param  array  $data
+     * @return void
+     */
     public static function setTemp(string $driver, int $type, string $openid, array $data): void
     {
         $key = sprintf(self::TEMP_KEY, $driver, $openid);
