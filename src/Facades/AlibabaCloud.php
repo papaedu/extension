a82c827e11f1609b5acec5196cc71090e7f8f747
zhangchaojun <yoki.zhang@papaenglish.cn>
2023-07-13 14:14:42 +0800

    feat: 移除阿里云图片审核模块

--- src/Facades/AlibabaCloud.php
+++ src/Facades/AlibabaCloud.php
@@ -3,7 +3,6 @@
 namespace Papaedu\Extension\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use Papaedu\Extension\AlibabaCloud\Green\GreenClient;
 use Papaedu\Extension\AlibabaCloud\Sts\StsClient;
 
 class AlibabaCloud extends Facade
@@ -11,11 +10,6 @@ class AlibabaCloud extends Facade
     protected static function getFacadeAccessor(): string
     {
         return 'alibaba_cloud.oss';
-    }
-
-    public static function green(): GreenClient
-    {
-        return app('alibaba_cloud.green');
     }
 
     public static function sts(): StsClient
